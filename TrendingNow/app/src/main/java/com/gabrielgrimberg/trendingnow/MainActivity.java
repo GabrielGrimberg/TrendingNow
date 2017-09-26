47ddce4d44ccaaf2eeccb4d80138a73b12f03385
Gabriel Grimberg <gabby.gr97@hotmail.com>
2017-09-26 09:29:56 +0100

    Improved BufferReader + Error Handling

--- TrendingNow/app/src/main/java/com/gabrielgrimberg/trendingnow/MainActivity.java
+++ TrendingNow/app/src/main/java/com/gabrielgrimberg/trendingnow/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -46,7 +47,7 @@ public class MainActivity extends AppCompatActivity
             //Debug level.
             Log.d(TAG, "doInBackground: Starts with " + strings[0]);
 
-            String rssFeed = downlaodXML(strings[0]);
+            String rssFeed = downloadXML(strings[0]);
 
             if(rssFeed == null)
             {
@@ -69,13 +70,42 @@ public class MainActivity extends AppCompatActivity
                 int response = connection.getResponseCode();
                 Log.d(TAG, "downloadXML: The response code was: " + response);
 
-                InputStream inputStream = connection.getInputStream();
-                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-                BufferedReader reader = new BufferedReader(inputStreamReader);
+//              InputStream inputStream = connection.getInputStream();
+//              InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+//              BufferedReader reader = new BufferedReader(inputStreamReader);
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream() ) );
+
+                int charsRead;
+                char[] inputBuffer = new char[500];
+
+                //Loop while end of input string reached.
+                while(true)
+                {
+                    charsRead = reader.read(inputBuffer);
+
+                    //Singals end of the string data.
+                    if(charsRead < 0)
+                    {
+                        break;
+                    }
+
+                    //Number of characters read from the string.
+                    //Check if there is something to read.
+                    if(charsRead > 0)
+                    {
+                        xmlResults.append(String.copyValueOf(inputBuffer, 0, charsRead) );
+                    }
+                }
+                reader.close();
             }
             catch(MalformedURLException e)
             {
-                Log.e(TAG, "downloadXML: Invalid URL. " + e.getMessage());
+                Log.e(TAG, "downloadXML: Invalid URL. " + e.getMessage()); //Invalid link error.
+            }
+            catch(IOException e)
+            {
+                Log.e(TAG, "downloadXML: IO Exception reading data: " + e.getMessage());
             }
         }
 
